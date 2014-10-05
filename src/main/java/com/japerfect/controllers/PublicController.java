9a59f7fa5b285ac9c961adca4251faa8151d31c4
Francis Manoj Fernando <manojfdo007@gmail.com>
2014-10-05 16:04:59 +0530

    Added Spring 3.2.5, edit web.xml , add frontController-servlet and publicController

---
+++ src/main/java/com/japerfect/controllers/PublicController.java
@@ -0,0 +1,13 @@
+package com.japerfect.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PublicController {
+
+	@RequestMapping("/index")
+	public String home(){
+		return "index.jsp";
+	}
+}
