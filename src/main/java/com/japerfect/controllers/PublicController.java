4a10f7016739f04cb523713f0d1055d2514d3b4a
Francis Manoj Fernando <manojfdo007@gmail.com>
2014-10-05 19:13:58 +0530

    add applicationContext.xml and moved the Titles Configurations

--- src/main/java/com/japerfect/controllers/PublicController.java
+++ src/main/java/com/japerfect/controllers/PublicController.java
@@ -3,7 +3,7 @@ package com.japerfect.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
+@Controller	
 public class PublicController {
 
 	@RequestMapping("/index")
